6116d6196d997c8ca80267fffd7045081c060b6e
rhakt <xreinf.fnk@gmail.com>
2016-05-09 15:48:21 +0900

    sync

--- apply.sh
+++ apply.sh
@@ -3,5 +3,6 @@ if [ $? -eq 0 ] ; then
   cp Brewfile ~
   cp .zshrc ~
   cp -r .zsh ~
+  cp -r .latexmkrc ~
   brew brewdle
 fi
--- sync.sh
+++ sync.sh
@@ -1,6 +1,7 @@
 brew brewdle dump --force
 cp ~/.zshrc .
 cp -r ~/.zsh .
+cp -r ~/.latexmkrc .
 git commit -a -m "sync"
 if [ $? -eq 0 ] ; then
   git push
